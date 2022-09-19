fc82b62f3457b2ed492394bebd98ddcf85230216
Xavier <xaviercotedaviau@gmail.com>
2022-09-19 17:21:50 -0500

    Ajout des propriétés et du tableau contenant les blocktypes dans BlockArray3D

--- GoldDigger/blockarray3d.cpp
+++ GoldDigger/blockarray3d.cpp
@@ -3,24 +3,31 @@
 #include "define.h"
 #include <iostream>
 
-BlockArray3D::BlockArray3D()
+BlockArray3D::BlockArray3D(int x, int y, int z) : m_x(x), m_y(y), m_z(z)
 {
-	std::cout << "ctor blockarray" << std::endl;
+	m_blocks = new BlockType[m_x*m_y*m_z];
+	for (int i = 0; i < (m_x * m_y * m_z); i++)
+		m_blocks[i] = BTYPE_AIR;
 }
 
 BlockArray3D::~BlockArray3D()
 {
+	delete[] m_blocks;
+	std::cout << "dtor" << std::endl; // TEST
 }
 
 void BlockArray3D::Set(int x, int y, int z, BlockType type)
 {
+	m_blocks[x + (z * m_x) + (y * m_z * m_x)] = type;
 }
 
 BlockType BlockArray3D::Get(int x, int y, int z) const
 {
-	return BlockType();
+	return m_blocks[x + (z * m_x) + (y * m_z * m_x)];
 }
 
 void BlockArray3D::Reset(BlockType type)
 {
+	for (int i = 0; i < (m_x * m_y * m_z); i++)
+		m_blocks[i] = type;
 }
--- GoldDigger/main.cpp
+++ GoldDigger/main.cpp
@@ -20,5 +20,9 @@ int main()
 	std::cout << "Durability: " << b2.GetDurability() << std::endl;
 	b2.Show();
 
-	BlockArray3D* world = new BlockArray3D;
+	BlockArray3D world(16,128,16);
+
+	std::cout << world.Get(1, 1, 1) << std::endl;
+	world.Set(1, 1, 2, BTYPE_DIRT);
+	std::cout << world.Get(1, 1, 2) << std::endl;
 }
