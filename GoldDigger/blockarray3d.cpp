2197f646aace826136c3ccdd6a6d4daa89891d88
Xavier <xaviercotedaviau@gmail.com>
2022-09-20 12:26:13 -0500

    Ajout d'un constructeur de copie pour BlockArray3D

--- GoldDigger/blockarray3d.cpp
+++ GoldDigger/blockarray3d.cpp
@@ -16,6 +16,13 @@ BlockArray3D::~BlockArray3D()
 	std::cout << "dtor" << std::endl; // TEST
 }
 
+BlockArray3D::BlockArray3D (const BlockArray3D& autre) : m_blocks(autre.m_blocks)
+{
+	m_blocks = new BlockType[m_x * m_y * m_z];
+	for (int i = 0; i < (m_x * m_y * m_z); i++)
+		m_blocks[i] = autre.m_blocks[i];
+}
+
 void BlockArray3D::Set(int x, int y, int z, BlockType type)
 {
 	m_blocks[x + (z * m_x) + (y * m_z * m_x)] = type;
