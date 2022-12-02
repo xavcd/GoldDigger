d2205fc5e8b72249599701630047bceba5282dc2
Olivier Charron <2089149@etu.cegepsth.qc.ca>
2022-12-02 09:58:42 -0500

    Génération du monde avec Perlin terminé!

--- GoldDigger/engine.cpp
+++ GoldDigger/engine.cpp
@@ -4,7 +4,7 @@
 #include <cmath>
 #include <iostream>
 
-Engine::Engine() : m_player(Vector3f(50.f, 120.8f, 50.f), 0.f, 0.f), m_textureAtlas(BTYPE_LAST), m_array2d((VIEW_DISTANCE * 2) / CHUNK_SIZE_X, (VIEW_DISTANCE * 2) / CHUNK_SIZE_Z)
+Engine::Engine() : m_player(Vector3f(50.f, 90.f, 50.f), 0.f, 0.f), m_textureAtlas(BTYPE_LAST), m_array2d((VIEW_DISTANCE * 2) / CHUNK_SIZE_X, (VIEW_DISTANCE * 2) / CHUNK_SIZE_Z)
 {
 }
 
